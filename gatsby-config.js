d9519a190ad9d4657106728847790b74876263f7
hong <gmm117@naver.com>
2021-12-29 00:20:19 +0900

    feat : SSR 처리
     - styled-components 라이브러리에 ssg 도입을 위한 라이브러리 추가
     - 포스트 컴포넌트 반응형 스타일 적용

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,12 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
