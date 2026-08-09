4b48d08006e3682711f843bbe5451f505e7522e5
seungahhong <seungahhong@users.noreply.github.com>
2026-08-09 11:38:38 +0000

    deploy: e49f11a5018924af77239f5a56e3ec85b5471441

---
+++ _next/static/-OXy1pKERk4WQAewVbxNn/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[lang]","\u002F[lang]\u002Fabout","\u002F[lang]\u002Fposts","\u002F[lang]\u002Fposts\u002F[slug]","\u002F[lang]\u002Ftags","\u002Fblog\u002F[year]\u002F[month]\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
